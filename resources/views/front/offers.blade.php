37a3bba807c212526773e7ea0fd773ca06df091d
netwarp <resethread@gmail.com>
2020-03-28 15:20:13 +0100

    Update offers.blade.php

--- resources/views/front/offers.blade.php
+++ resources/views/front/offers.blade.php
@@ -56,16 +56,16 @@
                     <div class="card-body">
                         <form action="#" method="get">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="location" placeholder="Lieux" value="{{ Request::get('location') }}">
+                                <input type="text" class="form-control" name="city" placeholder="Lieux" value="{{ Request::get('city') ?? '' }}">
                             </div>
                             <div class="form-group">
-                                <select name="type" class="form-control">
-                                    <option value="#">Acheter</option>
-                                    <option value="#">Louer</option>
+                                <select name="service" class="form-control">
+                                    <option value="acheter" {{ Request::get('service') == 'acheter' ? 'selected' : '' }}>Acheter</option>
+                                    <option value="louer" {{ Request::get('service') == 'louer' ? 'selected' : '' }}>Louer</option>
                                 </select>
                             </div>
                             <div class="form-group">
-                                <input type="number" class="form-control" name="rooms" placeholder="Pièces" value="{{ Request::get('rooms') ?? '' }}">
+                                <input type="number" class="form-control" name="rooms" placeholder="Nombre de pièces minimum" value="{{ Request::get('rooms') ?? '' }}">
                             </div>
                             <div class="form-group">
                                 <input type="number" class="form-control" name="min_surface" placeholder="Minimum mètres carrés" value="{{ Request::get('min_surface') }}">
@@ -74,7 +74,16 @@
                                 <label for="type"><b>Type de biens</b></label>
                                 @foreach($types as $item)
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="{{ $item }}">
+                                        @php
+                                            $checked = false;
+                                            $array = Request::get('types');
+                                            if ($array) {
+                                                if (in_array($item, $array)) {
+                                                    $checked = true;
+                                                }
+                                            }
+                                        @endphp
+                                        <input type="checkbox" class="form-check-input" id="{{ $item }}" name="types[]" value="{{ $item }}" {{ $checked ? 'checked' : '' }}>
                                         <label class="form-check-label" for="{{ $item }}">{{ $item }}</label>
                                     </div>
                                 @endforeach
@@ -83,16 +92,34 @@
                                 <label for="amenities"><b>Commodités</b></label>
                                 @foreach($amenities as $item)
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="{{ $item }}">
+                                        @php
+                                            $checked = false;
+                                            $array = Request::get('amenities');
+                                            if ($array) {
+                                                if (in_array($item, $array)) {
+                                                    $checked = true;
+                                                }
+                                            }
+                                        @endphp
+                                        <input type="checkbox" class="form-check-input" id="{{ $item }}" name="amenities[]" value="{{ $item }}" {{ $checked ? 'checked' : '' }}>
                                         <label class="form-check-label" for="{{ $item }}">{{ $item }}</label>
                                     </div>
                                 @endforeach
                             </div>
                             <div class="form-group">
-                                <label for="amenities"><b>Sécurités</b></label>
+                                <label for="securities"><b>Sécurités</b></label>
                                 @foreach($securities as $item)
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="{{ $item }}">
+                                        @php
+                                            $checked = false;
+                                            $array = Request::get('securities');
+                                            if ($array) {
+                                                if (in_array($item, $array)) {
+                                                    $checked = true;
+                                                }
+                                            }
+                                        @endphp
+                                        <input type="checkbox" class="form-check-input" id="{{ $item }}" name="securities[]" value="{{ $item }}" {{ $checked ? 'checked' : '' }}>
                                         <label class="form-check-label" for="{{ $item }}">{{ $item }}</label>
                                     </div>
                                 @endforeach
@@ -105,19 +132,15 @@
                 </div>
             </div>
             <div class="col-md-8">
-                @for($i = 0; $i < 10; $i++)
                 @forelse($properties as $property)
                     <div class="card mb-2">
                         <div class="row">
                             <div class="col-md">
                                 <a href="{{ action('Front\FrontController@getProperty', $property->slug) }}">
-                                    <img src="{{ $property->preview() ?? '' }}" alt="" class="card-img-top">
+                                    <img src="{{ $property->preview() ?? '' }}" alt="" class="card-img-top" title="{{ $property->title ?? '' }}">
                                 </a>
                             </div>
                             <div class="col-md">
-                                <h2 class="h3">
-                                    {{ $property->title ?? '' }}
-                                </h2>
                                 <div class="h5">
                                     {{ $property->city ?? '' }}
                                     {{ $property->department ?? '' }}
@@ -133,6 +156,7 @@
                                     <a href="{{ action('Front\FrontController@getProperty', $property->slug) }}" class="btn btn-primary mt-4">
                                         Voir l'offre
                                     </a>
+                                    {{ $property->service ?? '' }}
                                 </div>
                             </div>
                         </div>
@@ -140,7 +164,7 @@
                 @empty
                     Pas d'offres selon vos critères
                 @endforelse
-                @endfor
+                {{ $properties->links() }}
             </div>
         </div>
     </div>
